70b033e8f68e1bb9d917717004202eb07d0fb58e
StrudyStructure <austinloyd@proton.me>
2024-08-19 10:55:55 -0500

    updated main.py with pygame.init() and loop

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # This allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame # type: ignore
+from constants import *
+
 
 def main():
-    print("Starting asteroids!")
-
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill("black")
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Starting asteroids!")
+                print(f"Screen width: {SCREEN_WIDTH}")
+                print(f"Screen height: {SCREEN_HEIGHT}")
+                return
 
 
 
