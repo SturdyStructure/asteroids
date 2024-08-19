05f2a4e77b3325099c97f0f5fb73b77e74003953
StrudyStructure <austinloyd@proton.me>
2024-08-19 12:06:53 -0500

    added draw player and rotate player

--- main.py
+++ main.py
@@ -3,21 +3,31 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill("black")
-    pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("Starting asteroids!")
-                print(f"Screen width: {SCREEN_WIDTH}")
-                print(f"Screen height: {SCREEN_HEIGHT}")
-                return
+               return 
+                
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip() 
 
+        # limit the framerate to 60 FPS       
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
